d9c7cf8532fdee6a946903eefdf32b4d2aa97651
jollywonka123 <81097869+jollywonka123@users.noreply.github.com>
2021-05-14 17:17:01 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,8 @@
+Manual: https://telegra.ph/Zarabatyvaem-na-CHekah-BTC-04-15Manual
+Avtor: https://t.me/LORD_CODE
+
+
+
+
+
+
---
+++ config.py
@@ -0,0 +1,4 @@
+idchat=-1001317805504 #айди чата
+api_hash='2f3b9a24fed91d341bd4c2b5dbd0c7f8' #читайте мануал
+api_id='5107121' #читайте мануал
+username='REDKILLER33' #ваш ник без @
